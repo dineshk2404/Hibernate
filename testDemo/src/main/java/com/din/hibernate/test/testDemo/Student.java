a9f478b52dc45c236473236472258946850a925a
Dinesh Katare <39292835+dineshk2404@users.noreply.github.com>
2018-05-23 11:33:39 +0530

    Add files via upload
    first commit

---
+++ testDemo/src/main/java/com/din/hibernate/test/testDemo/Student.java
@@ -0,0 +1,5 @@
+package com.din.hibernate.test.testDemo;
+
+public class Student {
+
+}
---
+++ testDemo/src/main/java/com/din/hibernate/test/testDemo/TestStudent.java
@@ -0,0 +1,8 @@
+package com.din.hibernate.test.testDemo;
+
+public class TestStudent {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
